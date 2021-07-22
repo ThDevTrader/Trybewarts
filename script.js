858dc1b857a592d787510a7dbff48793eab4ab8a
Matheus Vaz <matheus.vaz@outlook.com>
2021-07-22 15:38:46 -0300

    requisito 3

---
+++ script.js
@@ -0,0 +1,11 @@
+const btnLogin = document.getElementById('btnLogin');
+const login = document.getElementById('login');
+const senha = document.getElementById('senha');
+
+btnLogin.addEventListener('click', function () {
+  if (login.value === 'tryber@teste.com' && senha.value === '123456') {
+    alert("Olá, Tryber!");
+  } else {
+    alert("Login ou senha inválidos.");
+  }
+});
