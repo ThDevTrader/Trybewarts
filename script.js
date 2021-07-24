e855de5fc7054bb321267d81b7723d0ef6d5d57f
ThDevTrader <thiago.so@hotmail.com>
2021-07-24 08:09:23 -0300

    Requisito 20

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const login = document.getElementById('login');
 const senha = document.getElementById('senha');
 const btnSubmit = document.getElementById('submit-btn');
 const check = document.getElementById('agreement');
+const text = document.querySelector('#textarea');
 
 btnLogin.addEventListener('click', () => {
   if (login.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -21,3 +22,14 @@ check.addEventListener('click', (event) => {
     btnSubmit.disabled = false;
   }
 });
+
+text.addEventListener('keyup', (event) => { //seguindo instruções da página: https://www.horadecodar.com.br/2020/12/09/contador-de-caracteres-em-uma-textarea-com-javascript/
+  const textLength = text.value.length;     //com o uso de "keypress" o valor não decrementava. "keydown" impedia o uso de backspace e travou a digitação ao final
+  const maxCharacters = 500;                //então, seguindo ideias deste video: https://www.youtube.com/watch?v=0X12LKGWcmE&ab_channel=LucasSilva
+  const left = maxCharacters - textLength;  //e da página https://api.jquery.com/keypress/ o "keyup" resolveu o problema
+  const counter = document.querySelector('#counter');
+  counter.innerHTML = left;
+  if (textLength >= maxCharacters){
+    event.preventDefault();
+  }
+})
